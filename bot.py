6cb3681c69370e7eb2727a90ee399c8dd2cfffd9
Hamza Bora <51544084+Hamza-Bora@users.noreply.github.com>
2020-09-08 01:19:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,6 @@ timer = sys.argv[1]
 bot = Bot()
 bot.login(username = "yourusername", password = "yourpassword")
 
-bot.upload_photo("/home/user/Hermes/resim"+str(timer)+".png", caption = "Katiliminiz icin tesekkur ederiz. Hamza'nin selami var..")
+bot.upload_photo("/home/user/Hermes/image"+str(timer)+".png", caption = "Katiliminiz icin tesekkur ederiz. Hamza'nin selami var..")
 
                     # directory of the file        |                        | Description of the photo to upload  |
